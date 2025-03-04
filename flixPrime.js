b9d3d9cdda4a0d9021c45d6cf20a1227754d92e7
FANKEP POUEME <107454134+saint-ines@users.noreply.github.com>
2025-03-04 15:10:07 +0100

    update javaScript

--- flixPrime.js
+++ flixPrime.js
@@ -8,3 +8,24 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollTop = document.querySelector('.scroll-top');
+
+    // afficher le boutton de retour a la page
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+            scrollTop.classList.add('visible');
+        } else {
+            scrollTop.classList.remove('visible');
+        }
+    });
+
+    // boutton pour le retoure au debut de page
+    scrollTop.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
